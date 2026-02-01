dc62e5d1fc30ee29dffa0d433aeecfabacf36fa8
MiracleN7 <143273892+MiracleN7@users.noreply.github.com>
2026-02-01 23:52:19 +0700

    กกกก

--- settings.js
+++ settings.js
@@ -1,17 +1,9 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
 
-    // ⭐ เพิ่มบรรทัดนี้
     httpRoot: "/",
-
     httpAdminRoot: "/red",
     httpNodeRoot: "/api",
 
-    disableEditor: false,
-
-    editorTheme: {
-        projects: {
-            enabled: false   // ให้ตรงกับที่ Render แสดง
-        }
-    }
+    disableEditor: false
 };
