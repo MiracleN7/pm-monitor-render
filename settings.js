ae0fb04e5b7136e450289b5d40d52f3c59f82288
MiracleN7 <143273892+MiracleN7@users.noreply.github.com>
2026-02-02 00:07:12 +0700

    cscspath

--- settings.js
+++ settings.js
@@ -1,4 +1,9 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
+
+    // ⭐ บังคับเปิด editor ที่ root
+    httpAdminRoot: "/",
+
+    // (ไม่จำเป็น แต่ใส่ให้ชัด)
     disableEditor: false
 };
