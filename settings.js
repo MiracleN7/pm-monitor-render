87a8c6364f4c38a94429498fc73d98fff3fab89a
MiracleN7 <143273892+MiracleN7@users.noreply.github.com>
2026-02-02 00:24:16 +0700

    ดกดดเเเเ

--- settings.js
+++ settings.js
@@ -1,8 +1,12 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
 
-    httpAdminRoot: "/editor",
+    // ⭐ เปิด editor แบบ explicit
+    httpAdminRoot: "/",
+
+    // ⭐ แยก http node ออกไป (กันชน)
     httpNodeRoot: "/api",
 
+    // ⭐ บังคับเปิด editor
     disableEditor: false
 };
