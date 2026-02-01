078cb334d9c4c0343adf6d076424d22f53fcf66b
MiracleN7 <143273892+MiracleN7@users.noreply.github.com>
2026-02-02 00:18:28 +0700

    ffffg

--- settings.js
+++ settings.js
@@ -1,9 +1,8 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
 
-    // ⭐ บังคับเปิด editor ที่ root
-    httpAdminRoot: "/",
+    httpAdminRoot: "/editor",
+    httpNodeRoot: "/api",
 
-    // (ไม่จำเป็น แต่ใส่ให้ชัด)
     disableEditor: false
 };
